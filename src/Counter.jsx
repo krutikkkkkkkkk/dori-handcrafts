028b513c6dee1e0033bb9505e2338bb895b995e5
krutikkkkkkkkk <rautkrutik013@gmail.com>
2024-05-22 14:34:12 +0530

    updated counter time

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,19 +1,19 @@
 import React, { useEffect, useRef } from 'react';
 import { CountUp } from 'countup.js';
 
-const Counter = ({ id, endVal, trigger }) => {
+const Counter = ({ id, endVal, trigger, duration = 4 }) => {
   const counterRef = useRef(null);
 
   useEffect(() => {
     if (trigger) {
-      const countUp = new CountUp(counterRef.current, endVal);
+      const countUp = new CountUp(counterRef.current, endVal, { duration });
       if (!countUp.error) {
         countUp.start();
       } else {
         console.error(countUp.error);
       }
     }
-  }, [trigger, endVal]);
+  }, [trigger, endVal, duration]);
 
   return <h2 ref={counterRef} className="text-3xl md:text-[3rem] font-bold font-count">{endVal}+</h2>;
 };
